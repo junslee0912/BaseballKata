544d61977d1fc757fc43e09537cec2279e926eb5
Junsecuk <junslee0912@gmail.com>
2025-07-23 12:59:05 +0900

    [refactor] p.37

--- BaseballKata/BaseballKata/baseball_test.cpp
+++ BaseballKata/BaseballKata/baseball_test.cpp
@@ -2,19 +2,23 @@
 #include "baseball.cpp"
 #include "string"
 
-TEST(BaseballGame, TC1)
-{
-	EXPECT_EQ(1, 1);
-}
-
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched)
-{
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(std::string("12")), length_error);
-}
+	void assertIllegalArgument(string guessNumber)
+	{
+		try {
+			game.guess(string("12s"));
+			FAIL();
+		}
+		catch (exception e) {
+			//PASS
+		}
+	}
+};
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar)
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase)
 {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
