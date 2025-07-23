efbd21e5e9137ae734278fb1f7ce0bdfe31840d0
Junsecuk <junslee0912@gmail.com>
2025-07-23 13:00:50 +0900

    [feature] p.39

--- BaseballKata/BaseballKata/baseball.cpp
+++ BaseballKata/BaseballKata/baseball.cpp
@@ -14,5 +14,10 @@ public:
 				throw invalid_argument("Must be number.");
 			}
 		}
+
+		if (guessNumber[0] == guessNumber[1] || guessNumber[0] == guessNumber[2] || guessNumber[2] == guessNumber[1])
+		{
+			throw invalid_argument("Must not have the same number.");
+		}
 	}
 };
