e4723731d43c5aca5ed9525da63cf6cce484f31b
Junsecuk <junslee0912@gmail.com>
2025-07-23 12:50:17 +0900

    [feature] p.27

--- BaseballKata/BaseballKata/baseball_test.cpp
+++ BaseballKata/BaseballKata/baseball_test.cpp
@@ -1,7 +1,14 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+#include "string"
 
 TEST(BaseballGame, TC1)
 {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched)
+{
+	Baseball game;
+	EXPECT_THROW(game.guess(std::string("12")), length_error);
 }
