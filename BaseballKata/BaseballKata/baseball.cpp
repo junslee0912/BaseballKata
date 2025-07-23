ac57800806c7f774c12ff2cec00cc14ab4ca7673
Junsecuk <junslee0912@gmail.com>
2025-07-23 13:14:40 +0900

    [refactor] p.50

--- BaseballKata/BaseballKata/baseball.cpp
+++ BaseballKata/BaseballKata/baseball.cpp
@@ -1,22 +1,38 @@
 #include <stdexcept>
 using namespace std;
 
-class Baseball {
+struct GuessResult
+{
+	bool solved;
+	int strikes;
+	int balls;
+};
+
+class Baseball 
+{
 public:
-	void guess(const string& guessNumber) {
+	explicit Baseball(const string& question) : question{ question } {}
+	GuessResult guess(const string& guessNumber) 
+	{
 		assertIllegalArgument(guessNumber);
+		if (guessNumber == question)
+		{
+			return { true,3,0 };
+		}
+		return { false, 0, 0 };
 	}
 	void assertIllegalArgument(const std::string& guessNumber)
 	{
-		if (guessNumber.length() != 3) {
+		if (guessNumber.length() != 3) 
+		{
 			throw length_error("Must be three letters.");
 		}
 
 		for (char ch : guessNumber)
 		{
-			if (ch >= '0' && ch <= '9') {
-				throw invalid_argument("Must be number.");
-			}
+			if (ch >= '0' && ch <= '9') continue;
+			throw invalid_argument("Must be number.");
+			
 		}
 
 		if (isDuplicatedNumber(guessNumber))
@@ -28,4 +44,7 @@ public:
 	{
 		return guessNumber[0] == guessNumber[1] || guessNumber[0] == guessNumber[2] || guessNumber[2] == guessNumber[1];
 	}
+
+private:
+	string question;
 };
--- BaseballKata/BaseballKata/baseball_test.cpp
+++ BaseballKata/BaseballKata/baseball_test.cpp
@@ -2,16 +2,19 @@
 #include "baseball.cpp"
 #include "string"
 
-class BaseballFixture : public testing::Test {
+class BaseballFixture : public testing::Test 
+{
 public:
-	Baseball game;
+	Baseball game{ "123" };
 	void assertIllegalArgument(string guessNumber)
 	{
-		try {
-			game.guess(string("12s"));
+		try 
+		{
+			game.guess(guessNumber);
 			FAIL();
 		}
-		catch (exception e) {
+		catch (exception e) 
+		{
 			//PASS
 		}
 	}
@@ -22,4 +25,13 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase)
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
 	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber)
+{
+
+	GuessResult result = game.guess("123");
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
